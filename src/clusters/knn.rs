09df2028e562e69f655e0273d19fff73b8772242
GiovaniGitHub <giovani.angelo@gmail.com>
2023-01-08 15:19:09 -0500

    change knn to uses DMatrix

--- src/clusters/knn.rs
+++ src/clusters/knn.rs
@@ -1,13 +1,10 @@
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
-use smartcore::linalg::{
-    naive::dense_matrix::DenseMatrix,
-    BaseMatrix, BaseVector,
-};
+use nalgebra::DMatrix;
 
 pub struct KNN {
-    pub x: DenseMatrix<f32>,
-    pub y: DenseMatrix<f32>,
+    pub x: DMatrix<f32>,
+    pub y: DMatrix<f32>,
     pub n_neighborhood: usize,
 }
 
@@ -26,7 +23,7 @@ impl NeighborhoodItem {
 }
 
 impl KNN {
-    pub fn new(x: DenseMatrix<f32>, y: DenseMatrix<f32>, n_neighborhood: usize) -> KNN {
+    pub fn new(x: DMatrix<f32>, y: DMatrix<f32>, n_neighborhood: usize) -> KNN {
         KNN {
             x,
             y,
@@ -34,25 +31,35 @@ impl KNN {
         }
     }
 
-    pub fn predict(&mut self, v: Vec<f32>) -> String {
-        let mut neighborhood_list = Vec::with_capacity(self.x.shape().0);
-        for i in 0..self.x.shape().0 {
-            let dist: f32 = self.x.get_row(i).sub(&v).iter().map(|r| r.powf(2.0)).sum();
-            let mut neighborhood_item = NeighborhoodItem::new();
+    pub fn predict(&mut self, x: DMatrix<f32>) -> Vec<String> {
+        let mut y_hat = Vec::new();
+        for idx in 0..x.nrows() {
+            let mut neighborhood_list = Vec::with_capacity(self.x.shape().0);
+            for i in 0..self.x.shape().0 {
+                let dist: f32 = (self.x.row(i) - x.row(idx))
+                    .iter()
+                    .map(|r| r.powf(2.0))
+                    .sum();
+                let mut neighborhood_item = NeighborhoodItem::new();
 
-            neighborhood_item.dist = dist;
-            neighborhood_item.class = self.y.get_row(i)[0];
+                neighborhood_item.dist = dist;
+                neighborhood_item.class = self.y[(i, 0)];
 
-            neighborhood_list.push(neighborhood_item);
+                neighborhood_list.push(neighborhood_item);
+            }
+
+            neighborhood_list
+                .sort_by(|a: &NeighborhoodItem, b| a.dist.partial_cmp(&b.dist).unwrap());
+
+            let mut m: HashMap<String, usize> = HashMap::new();
+            for ii in 0..self.n_neighborhood {
+                *m.entry(neighborhood_list[ii].class.to_string())
+                    .or_default() += 1;
+            }
+
+            y_hat.push(m.iter().max_by_key(|entry| entry.1).unwrap().0.clone());
         }
 
-        neighborhood_list.sort_by(|a: &NeighborhoodItem, b| a.dist.partial_cmp(&b.dist).unwrap());
-
-        let mut m: HashMap<String, usize> = HashMap::new();
-        for ii in 0..self.n_neighborhood {
-            *m.entry(neighborhood_list[ii].class.to_string()).or_default() += 1;
-        }
-
-        return m.iter().max_by_key(|entry| entry.1).unwrap().0.clone();
+        return y_hat;
     }
 }
